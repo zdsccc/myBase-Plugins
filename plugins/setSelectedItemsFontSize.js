3c2a6d7842b7970970989586afd17df19209d4d4
yesman <hiyesman@163.com>
2017-12-30 17:20:33 +0800

    所有选中的条目字号设置

--- plugins/setSelectedItemsFontSize.js
+++ plugins/setSelectedItemsFontSize.js
@@ -1,7 +1,3 @@
-/*
-*
-*
-*/
 
 var _lc=function(sTag, sDef){return plugin.getLocaleMsg(sTag, sDef);};
 var _lc2=function(sTag, sDef){return _lc(plugin.getLocaleID()+'.'+sTag, sDef);};
@@ -13,34 +9,51 @@ try{
     // 实例化CNyfDb类为对象nyf
     var nyf=new CNyfDb(-1); //-1: for the current one;
     // 对象nyf是否已mount数据库
-    if(nyf.isOpen()){
+    if(nyf.isOpen())
+    {
         // 数据库是否以只读方式打开的
-        if(!nyf.isReadonly()){
-            // 选中的分支
+        if(!nyf.isReadonly())
+        {
+            // 所有选中的条目
             var selectedInfoItems = plugin.getSelectedInfoItems(-1);
+            // 所有选中的条目的数量
             var selectedInfoItems_len = selectedInfoItems.length;
-
-            if(plugin.isContentEditable()){
-                // font size prompt
+            // 当前数据库中的html内容是否可编辑
+            if(plugin.isContentEditable(-1))
+            {
+                // key
                 var sCfgKey='gzhaha.SetFontSize';
-                var sSiz = prompt('Input Font Size (5-40):', localStorage.getItem(sCfgKey) || '16', 'Input Font Size');
-                if (sSiz) {
-                    if (sSiz >= 5 && sSiz <= 40) {
-                        for(var i=0;i<selectedInfoItems_len;i++) {
-                            // 获取条目文本
-                            alert(selectedInfoItems[i]+nyf.listFiles(selectedInfoItems[i]));
-                            var sCon = plugin.getTextContent(nyf.listFiles(selectedInfoItems[i]), true);
-                            // 设置的字体size保存到条目的ini文件
+                // 字号输入
+                var sSiz = prompt('Input Font Size (5-40):', localStorage.getItem(sCfgKey) || '16', 'SetSelectedItemsFontSize...');
+                // 字号存在的情况
+                if (sSiz)
+                {
+                    // 字号必须大于等于5并且小于等于40
+                    if (sSiz >= 5 && sSiz <= 40)
+                    {
+                        // 所有选中的条目遍历
+                        for(var i=0;i<selectedInfoItems_len;i++)
+                        {
+                            // 条目下的所有文件，包括附件，数组格式，不包括子文件夹下的文件
+                            var allFiles = nyf.listFiles(selectedInfoItems[i]);
+                            // _~_~_~notes.html文件的ssg路径
+                            var curFilePath = selectedInfoItems[i] + allFiles[0];
+                            alert(curFilePath);
+                            alert(sSiz);
+                            // 获取_~_~_~notes.html文件为html内容
+                            var sCon = plugin.getTextContent(curFilePath, true);
+                            // 设置的字体size保存到ini文件
                             localStorage.setItem(sCfgKey, sSiz);
                             //match px, pt, %
-                            var regx = /font-size:( |)\d{1,2}(|\.\d+)(| )pt|font-size:( |)\d{1,2}(|\.\d+)(| )px|font-size:( |)\d{1,3}(| )%/g;
+                            var regx = /font-size:( |)\d{0,1,2}(|\.\d+)(| )pt|font-size:( |)\d{0,1,2}(|\.\d+)(| )px|font-size:( |)\d{1,3}(| )%/g;
                             var html = sCon.replace(regx, 'font-size: ' + sSiz + 'pt');
-                            //2015.6.12 'setHTML' clears DOM entirely including UNDO stack, so it'd be worth to first save current modifications as a history revision for UNDOable;
-                            plugin.commitCurrentChanges();
                             // 替换条目的文本内容
-                            plugin.setTextContent(selectedInfoItems[i]+nyf.listFiles(selectedInfoItems[i]), html, true);
-                            plugin.setDomDirty(selectedInfoItems[i]+nyf.listFiles(selectedInfoItems[i]), true);
+                            plugin.setTextContent(curFilePath, html, true);
+                            // 为当前html标记dirty flag
+                            // plugin.setDomDirty(-1, true);
                         }
+                        // 如果当前数据库中当前html内容有dirty标记，提交改变
+                        plugin.commitCurrentChanges(-1);
                     }
                     else {
                         alert("Font Size should be 5-40!");
